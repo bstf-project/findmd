2fe148c9ac84ae15e12cf17df76e8a32e446b4ea
Bharat Singh <bksingh@g.cofc.edu>
2017-05-03 13:43:03 -0400

    Merging files again

--- js/general.js
+++ js/general.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-
-$('.search-button').click(function() {
+	
+$('#search-button').click(function() {
 
 var api_key = "789b28d7d74840d2eb449527c4d61127";
 var columbia_coord = "34.000,-81.035"; //Lat and Long values of Columbia, SC
@@ -16,7 +16,7 @@ var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?location='+c
 // Testing Ajax call here
 
 function displayDoctors (obj) {
-	return( obj.profile.first_name +  obj.profile.last_name +"<br/>" + "<img class=docimg src= " + obj.profile.image_url + ">" + "<br/>" + obj.profile.bio + "<br/>");
+	return( obj.profile.first_name + " " + obj.profile.last_name +"<br/>" + "<img src= " + obj.profile.image_url + ">" + "<br/>" + obj.profile.bio + "<br/>" + "<a href=\'htmlPages/doctorinsurance.html\' target=\'blank\'> <button>View Insurances</button> </a>" + "<br/>");
 }
 
 //Move the insurance stuff to a new js file. Idea is to separate like you would components in React
@@ -30,34 +30,37 @@ function displayInsurance (obj) {
 
 
 function testInject (data) {
-	document.getElementById('app').innerHTML += "<p class=\'info\''>" + data.data.map(displayDoctors).join(" ") + "</p>";
+	document.getElementById('app').innerHTML += "<p class=\'info\'>" + data.data.map(displayDoctors).join(" ") + "</p>";
 
 
 	// document.getElementById('app').innerHTML += "<p>" + data.data[0].profile.first_name + " " + data.data[0].profile.last_name + "</p>";
 	// document.getElementById('app').innerHTML += "<img src= " + data.data[0].profile.image_url + ">";
 	// document.getElementById('app').innerHTML += "<p>" + data.data[0].profile.bio + "</p>";
 
-	console.log(data.data.map(displayInsurance));
+	console.log(data.data);
+	//console.log(data.data.map(displayInsurance));
+
 	// data.data.profile.first_name =>first name of doctor
 	// data.data.profile.last_name => last name of doctor
 	// data.data.profile.bio => Brief description of doctor
 	// data.data[i].insurances => returns an array of insurances a doctor takes
 	// data.data[i].claims => returns an array of prices for doctor and what procedure
 }
-
+    
 		$.ajax({
 			url: resource_url,
 			success: function successHandler(data) {
 				testInject(data);
-			}
+			} 
 		});
-
+    
   });
-
-  $("#searchTerm").keypress(function(e) {
+  
+  $(".search-box").keypress(function(e) {
     if (e.which == 13) {
-      $("#search").click();
+      $("#search-button").click();
     }
   });
-
+  
 });
+
