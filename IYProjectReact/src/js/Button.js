730726a2463fd299ea9d793ec84566915c08953a
Bharat Singh <bksingh@g.cofc.edu>
2017-05-04 11:09:03 -0400

    Added the button click thing

---
+++ IYProjectReact/src/js/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/App.css';
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button {...this.props}>
+        click
+      </button>
+    );
+  }  
+}
+
+module.exports = Button;
---
+++ IYProjectReact/src/js/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../css/App.css';
+
+
+class Header extends React.Component {
+
+ render() {
+   return (
+	<div className = "title-search-wrapper">
+       <h3 className = "title-name">FindMD</h3>
+       <input type="text" className="search-field search-box" id="search-term" value="Columbia, SC"/>
+       <button {...this.props} id="search-button" className="search-button btn btn-primary" type="button">Search
+       </button>
+		<button className ="search-button btn btn-primary" type="button">Filter</button>
+    </div>
+   );
+ }
+
+} 
+
+module.exports = Header;
