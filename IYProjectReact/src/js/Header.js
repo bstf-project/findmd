6b8760a150361eac4c76d378af94642750a46f8b
rooftb <rooftb@email.sc.edu>
2017-05-05 13:36:43 -0400

    Added Latitude and Longitude Text Input and Changed the god awful background

--- IYProjectReact/src/js/Header.js
+++ IYProjectReact/src/js/Header.js
@@ -9,7 +9,9 @@ class Header extends React.Component {
 	<div className = "title-search-wrapper">
 
       <h1 className="title-name">FindMD</h1>
-          <input type="text" className="search-field search-box " id="search-term" value="Columbia, SC"/>
+          <input type="text" className="search-field search-box" id="search-term" placeholder="Longitude"/>
+          <input type="text" className="search-field search-box" id="search-term" placeholder="Latitude"/>
+
           <button {...this.props} id="search-button" className="search-button btn btn-primary" type="button">Search
           </button>
    		<button className ="search-button btn btn-primary" type="button">Filter</button>
