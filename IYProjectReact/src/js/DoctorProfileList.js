100e271fe1e85653b6d74cc6c546054494e5d6ae
rooftb <rooftb@email.sc.edu>
2017-05-16 20:38:54 -0400

    Incredible CSS changes. Things are looking up

--- IYProjectReact/src/js/DoctorProfileList.js
+++ IYProjectReact/src/js/DoctorProfileList.js
@@ -31,7 +31,7 @@ class DoctorProfileList extends React.Component {
 			api_key: '789b28d7d74840d2eb449527c4d61127',
 			amountReturned: defaultAmountReturned,
 			skip_limit: 2
-			
+
 		}
 
 		this.getLocation();
@@ -84,7 +84,7 @@ class DoctorProfileList extends React.Component {
 			.then(response => {this.setState({resultArr: response.data.data});
 
 			console.log(this.state.resultArr);
-			
+
 			console.log(this.state.resource_url + this.state.coordinates + ',' + this.state.distance + '&skip='+ this.state.skip_limit +'&limit='+this.state.amountReturned+'&user_key=' + this.state.api_key);
 		});
 
@@ -135,7 +135,7 @@ class DoctorProfileList extends React.Component {
 		})
 
 		this.APIcall();
-		
+
 	}
 
 	increaseRadius () {
@@ -153,8 +153,8 @@ class DoctorProfileList extends React.Component {
 
 			return (
 				<div>
+					<div className="wrap">
 					<div className="search-wrapper">
-					<button className="search-button btn-primary" onClick={this.increaseRadius}>Within {this.state.distance} miles</button>
 						<form>
 					        <input
 							className="search-box"
@@ -167,10 +167,12 @@ class DoctorProfileList extends React.Component {
 
 			      		</form>
 			      		<button className="search-button btn-primary" onClick={this.handleChange}>Find Doctors</button>
-						<button className="search-button btn-primary" onClick={this.addDoctor}>+</button>
+								<button className="search-button plus-radius btn-primary" onClick={this.increaseRadius}>Within {this.state.distance} miles</button>
+						<button className="search-button btn-primary" onClick={this.addDoctor}>+ Radius</button>
 
-		      		</div>
-
+		      		
+						</div>
+					</div>
 
 					<LeafletMap
 					centerLocation={this.state.coordinates}
@@ -181,7 +183,7 @@ class DoctorProfileList extends React.Component {
 					<DoctorProfiles
 					searchRadius={this.state.distance}
 					doctorData={this.state.resultArr}
-					/>					
+					/>
 				</div>
 			);
 
