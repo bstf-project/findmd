3efc1b0be654ce613cf4e3bb1c227141249f7df7
Bharat Singh <bksingh@g.cofc.edu>
2017-05-25 14:07:02 -0400

    Debugging DoctorProfileList

--- src/js/DoctorProfileList.js
+++ src/js/DoctorProfileList.js
@@ -94,11 +94,12 @@ class DoctorProfileList extends React.Component {
 
 			.then(response =>
 				{this.setState({resultArr: response.data.data});
-				{this.setState({showMedical: response.data.data.filter(function (obj) {return obj.specialties[0].category === 'medical'})})};
+				//Taking filters out for now => will add in later
+				// {this.setState({showMedical: response.data.data.filter(function (obj) {return obj.specialties[0].category === 'medical'})})};
 
-				{this.setState({showVision: response.data.data.filter(function (obj) {return obj.specialties[0].category === 'vision'})})};
+				// {this.setState({showVision: response.data.data.filter(function (obj) {return obj.specialties[0].category === 'vision'})})};
 
-				{this.setState({showDental: response.data.data.filter(function (obj) {return obj.specialties[0].category === 'dental'})})};
+				// {this.setState({showDental: response.data.data.filter(function (obj) {return obj.specialties[0].category === 'dental'})})};
 
 
 
